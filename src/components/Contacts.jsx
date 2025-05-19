4776d94e4fd210d2a677081d82205b7bea1cd304
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-05-19 21:59:42 +0200

    get contacts

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -4,42 +4,37 @@ import { Link } from "react-router-dom";
 import styles from "../styles/contacts.module.css";
 
 export default function Contacts() {
-    //Get users that logged user has chat with!!!!
+    const { API_URL } = useContext(AppContext);
+    const [contacts, setContacts] = useState([]);
+    useEffect(() => {
+        async function getUsers() {
+            try {
+                const req = await fetch(`${API_URL}/users/contacts`, {credentials: "include"});
+                if (!req.ok) {
+                    throw new Error("Failed to get the data")
+                }
+                const res = await req.json();
+                console.log(res.contacts);
+                setContacts(res.contacts);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        getUsers();
+    }, [])
 
-
-
-
-    // const { API_URL } = useContext(AppContext);
-    // const [users, setUsers] = useState([]);
-    // useEffect(() => {
-    //     async function getUsers() {
-    //         try {
-    //             const req = await fetch(`${API_URL}/users`, {credentials: "include"});
-    //             if (!req.ok) {
-    //                 throw new Error("Failed to get the data")
-    //             }
-    //             const res = await req.json();
-    //             console.log(res.users);
-    //             setUsers(res.users);
-    //         } catch (err) {
-    //             console.error(err);
-    //         }
-    //     }
-    //     getUsers();
-    // }, [])
-
-    // return (
-    //     users.length > 0 ? <ul className={styles.contactsList}>
-    //         {users.map((user, id) => {
-    //             return <li key={id}>
-    //                 <Link to={`/chat/${user.id}`}>
-    //                     {user.username}
-    //                     {user.status}
-    //                 </Link>
+    return (
+        contacts.length > 0 ? <ul className={styles.contactsList}>
+            {contacts.map((user, id) => {
+                return <li key={id}>
+                    <Link to={`/chat/${user.id}`}>
+                        {user.username}
+                        {user.status}
+                    </Link>
                     
-    //             </li>
-    //         })}
-    //     </ul> : <div>Loading...</div>
+                </li>
+            })}
+        </ul> : <div>You don't have any contacts Yet <Link to="/users">Try to find one</Link></div>
         
-    // )
+    )
 }
