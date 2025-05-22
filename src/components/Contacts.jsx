5f79577a348c58d28f7e833185bc27517448247b
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-05-22 14:01:31 +0200

    added search bar inside contacts

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -7,6 +7,7 @@ export default function Contacts() {
     const { API_URL,refreshTrigger } = useContext(AppContext);
     const [contacts, setContacts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [inputValue, setInputValue] = useState("");
     useEffect(() => {
         async function getUsers() {
             try {
@@ -25,13 +26,18 @@ export default function Contacts() {
         getUsers();
     }, [refreshTrigger])
 
+    const sortedContacts = !inputValue ? contacts : contacts.filter((contacts) => contacts.username.toLowerCase().includes(inputValue));
+
     return (
         loading ? <div>Loading...</div> : 
-        contacts.length > 0 ? <ul className={styles.contactsList}>
-            {contacts.map((user, id) => {
+            contacts.length > 0 ?
+                <ul className={styles.contactsList}>
+                    <input value={inputValue} type="text" placeholder="find contact" onChange={e => setInputValue(e.target.value.toLowerCase())} />
+            {sortedContacts.map((user, id) => {
                 return <li key={id}>
                     <Link to={`/chat/${user.id}`}>
-                        <p>{user.username}</p>
+                        <p>{user.firstName} {user.lastName}</p>
+                        <p>@{user.username}</p>
                        <p style={{color: user.status === "Offline" ? "red" : "green"}}>{user.status}</p>
                     </Link>
                     
