c02f824240d23e516d6f6f77d687f5b36587ae02
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-06-08 14:22:25 +0200

    replaced local server url with a public one

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import { useState, createContext } from "react";
 const AppContext = createContext();
 
 function Context({ children }) {
-    const API_URL = "http://localhost:8000"
+    const API_URL = "https://messaging-app-api-gkax.onrender.com"
     const [refreshTrigger, setRefreshTrigger] = useState(false);
     const [isRunning, setIsRunning] = useState(false);
 
